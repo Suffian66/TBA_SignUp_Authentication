fd0c396d7cae909959a4113e1328ce27771d7bd9
shari1434 <shari@wavestechnology.net>
2024-07-18 13:12:08 +0500

    nothing added

--- User.Management.Data/Models/Address.cs
+++ User.Management.Data/Models/Address.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace User.Management.Data.Models
